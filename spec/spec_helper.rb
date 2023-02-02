208bd28fa766bc535ec2b573455d1edc87394d49
codefactor-io <support@codefactor.io>
2023-02-02 19:43:05 +0000

    [CodeFactor] Apply fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,6 @@ VCR.config do |c|
 end
 
 token_file = File.expand_path('../.access_token', __FILE__)
-ACCESS_TOKEN = File.exists?(token_file) ? File.read(token_file).strip : 'faketoken'
+ACCESS_TOKEN = File.exist?(token_file) ? File.read(token_file).strip : 'faketoken'
 gauge_file = File.expand_path('../.gauge_id', __FILE__)
-GAUGE_ID = File.exists?(gauge_file) ? File.read(gauge_file).strip : 'fakegaugeid'
+GAUGE_ID = File.exist?(gauge_file) ? File.read(gauge_file).strip : 'fakegaugeid'
